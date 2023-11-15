ca1130e76261f247649c49e1729528e27f21581c
carson reeder <ca077181@ucf.edu>
2023-11-15 15:35:02 -0600

    Updated strategy details functionality and other improvements

--- app.js
+++ app.js
@@ -1,52 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
     let currentUser = null;
     const userStrategies = {
-        "demoUser": [{
-            strategyName: "Sample Strategy",
-            strategyDescription: "This is a sample strategy for demonstration purposes.",
-            targetInvestment: 10000,
-            currentInvestment: 5000 // This is a demo value for the current funding status
-        }]
+        "demoUser": [
+            // Pre-defined strategies
+            { strategyName: "Sample Strategy", strategyDescription: "This is a sample strategy for demonstration purposes.", targetInvestment: 10000, currentInvestment: 5000 },
+            { strategyName: "Conservative Strategy", strategyDescription: "A low-risk, long-term growth strategy focused on stability.", targetInvestment: 15000, currentInvestment: 7500 },
+            { strategyName: "Aggressive Growth Strategy", strategyDescription: "High-risk, high-reward strategy aiming for rapid growth.", targetInvestment: 20000, currentInvestment: 10000 }
+        ]
     };
 
+    // DOM elements
     const loginForm = document.getElementById('loginForm');
     const createStrategyForm = document.getElementById('createStrategyForm');
     const strategiesList = document.getElementById('strategiesList');
     const userProfile = document.getElementById('userProfile');
     const strategyDetails = document.getElementById('strategyDetails');
+    const strategySection = document.getElementById('strategySection');
+    const createStrategyContainer = document.getElementById('createStrategyContainer');
 
-    // Handle login
-    loginForm.addEventListener('submit', function (e) {
+    // Event Listeners
+    document.getElementById('toggleCreateStrategyForm').addEventListener('click', () => {
+        createStrategyContainer.style.display = createStrategyContainer.style.display === 'none' ? 'block' : 'none';
+    });
+
+    loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
         currentUser = document.getElementById('username').value;
         document.getElementById('loginSection').style.display = 'none';
-        document.getElementById('strategySection').style.display = 'block';
+        strategySection.style.display = 'block';
+        strategiesList.style.display = 'block';
         userProfile.innerHTML = `<p>Logged in as ${currentUser}</p>`;
         userProfile.style.display = 'block';
-
-        if (!userStrategies[currentUser]) {
-            userStrategies[currentUser] = [];
-        }
-
-        // Show the static strategy for demo
-        showStaticStrategy();
+        showStrategies(currentUser);
     });
 
-    // Handle strategy creation
-    createStrategyForm.addEventListener('submit', function (e) {
+    createStrategyForm.addEventListener('submit', (e) => {
         e.preventDefault();
-
         const strategyName = document.getElementById('strategyName').value;
         const strategyDescription = document.getElementById('strategyDescription').value;
         const targetInvestment = document.getElementById('targetInvestment').value;
-
-        userStrategies[currentUser].push({ strategyName, strategyDescription, targetInvestment });
-        strategiesList.innerHTML += `<p onclick="showStrategyDetails('${strategyName}', '${currentUser}')"><strong>${strategyName}</strong> by ${currentUser} - Target Investment: ${targetInvestment}</p>`;
-
-        createStrategyForm.reset();
+        addStrategy(currentUser, { strategyName, strategyDescription, targetInvestment });
     });
 
-    // Function to show user profile
+    // Helper Functions
+    function showStrategies(username) {
+        strategiesList.innerHTML = '';
+        const strategies = userStrategies[username] || userStrategies["demoUser"];
+        strategies.forEach(strategy => {
+            // Each strategy now has an onclick event to show its details
+            strategiesList.innerHTML += `<p onclick="showStrategyDetails('${strategy.strategyName}', '${username}')"><strong>${strategy.strategyName}</strong> by ${username} - Target Investment: ${strategy.targetInvestment}</p>`;
+        });
+    }
+
+    function addStrategy(username, strategy) {
+        userStrategies[username] = userStrategies[username] || [];
+        userStrategies[username].push(strategy);
+        showStrategies(username);
+    }
+
     window.showUserProfile = function(username) {
         const strategies = userStrategies[username].map(s => `<li>${s.strategyName} - Return: TBD</li>`).join('');
         userProfile.innerHTML = `<h3>${username}'s Strategies</h3><ul>${strategies}</ul>`;
@@ -54,20 +65,23 @@ document.addEventListener('DOMContentLoaded', () => {
         strategyDetails.style.display = 'none';
     };
 
-    // Function to show strategy details
-    window.showStrategyDetails = function(strategyName, username) {
-        const strategy = userStrategies[username].find(s => s.strategyName === strategyName);
-        strategyDetails.innerHTML = `
-            <p><strong>${strategy.strategyName}</strong> by <span onclick="showUserProfile('${username}')">${username}</span> - ${strategy.strategyDescription} - Target Investment: ${strategy.targetInvestment}</p>
-            <img src="chart.png" alt="Strategy Chart"> <!-- Add your chart image here -->
-        `;
-        strategyDetails.style.display = 'block';
+    window.showStrategyDetails = function(strategyName, demoUser) {
+        console.log("showStrategyDetails called for", strategyName, "by", username);
+        const allStrategies = Object.values(userStrategies).flat(); // Flatten all strategies
+        const strategy = allStrategies.find(s => s.strategyName === strategyName);
+
+        if (strategy) {
+            strategyDetails.innerHTML = `
+                <h3>${strategy.strategyName}</h3>
+                <p>Description: ${strategy.strategyDescription}</p>
+                <p>Target Investment: ${strategy.targetInvestment}</p>
+                <p>Current Investment: ${strategy.currentInvestment}</p>
+                <img src="chart.png" alt="Strategy Chart" class="strategy-chart"> <"C:/Users/carso/Fall23/Capstone/Project/chart.png">
+            `;
+            strategyDetails.style.display = 'block';
+        } else {
+            console.error("Strategy not found:", strategyName);
+        }
         userProfile.style.display = 'none';
     };
-
-    // Function to show the static strategy on login
-    function showStaticStrategy() {
-        const demoStrategy = userStrategies["demoUser"][0];
-        strategiesList.innerHTML = `<p onclick="showStrategyDetails('${demoStrategy.strategyName}', 'demoUser')"><strong>${demoStrategy.strategyName}</strong> by demoUser - Target Investment: ${demoStrategy.targetInvestment} - Current Investment: ${demoStrategy.currentInvestment}</p>`;
-    }
 });
